8c7e12f4014f25e7b2f09912844f20c89c05aef4
LopeKai <kaiqueoakley35@hotmail.com>
2022-06-14 20:49:43 -0300

    starting cards

--- src/pages/Releases/index.jsx
+++ src/pages/Releases/index.jsx
@@ -3,8 +3,17 @@ export function Releases() {
     return (
         <div className={styles.releases}>
             <div className={styles.releases__container}>
-                <div className="title">
+                <div className={styles.title}>
                     <h1>Lançamentos</h1>
+                </div>
+                <div className={styles.films}>
+                    <div className={styles.films__container}>
+                        <div className={styles.cards}>
+                            <div className={styles.cards}>
+                                
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
